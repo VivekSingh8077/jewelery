8bcd4387db86b86e638363478d24685928a7d2f3
Vivek Singh <viveksinghrajput8077@gmail.com>
2024-11-26 15:25:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+function toggleForms() {
+    const loginForm = document.getElementById('loginForm');
+    const signupForm = document.getElementById('signupForm');
+    
+    loginForm.classList.toggle('hidden');
+    signupForm.classList.toggle('hidden');
+}
+
+function login() {
+    const username = document.getElementById('loginUsername').value;
+    const password = document.getElementById('loginPassword').value;
+
+    // Here you would typically send the data to your server
+    alert(`Login attempted with Username: ${username} and Password: ${password}`);
+     window.location.href='homepage.html'
+}
+
+function signup() {
+    const username = document.getElementById('signupUsername').value;
+    const email = document.getElementById('signupEmail').value;
+    const password = document.getElementById('signupPassword').value;
+
+    // Here you would typically send the data to your server
+    alert(`Signup attempted with Username: ${username}, Email: ${email} and Password: ${password}`);
+}
+function emailnews(){
+    const email=document.getElementById('newsletterbutton')
+    if( email==" "){
+        alert("enter email first")
+    }else{
+        alert("email registered")
+    }
+}
+function addtocart(){
+    alert("Item added to cart!");
+}
+function purchase(){
+    alert("Purchase successful!");
+}
+function purchasebutton(){
+    window.location.href='form.html'
+}
